5820757ffb3f6a6380a49f1bb50d0568358462a1
Daria <dashka.kh@gmail.com>
2014-10-27 21:52:51 -0700

    Make sure db is updatedt before script exits

--- initDb.js
+++ initDb.js
@@ -15,62 +15,88 @@ function initDb() {
     connections.remove();
     maps.remove();
 
-    maps.insert({
-      _id: 1,
-      name: "Chicago"
-    });
-    maps.insert({
-      _id: 2,
-      name: "Washington D.C."
-    });
-    maps.insert({
-      _id: 3,
-      name: "USA"
-    });
-    maps.insert({
-      _id: 4,
-      name: "Mountain trails"
-    });
-
-    places.insert({
-      name: "Haiku Stairs",
-      location: {
-        lat: 21.400788,
-        lng: -157.821934
+    var todo = [
+      function(callback) {
+        maps.insert({
+          _id: 1,
+          name: "Chicago"
+        }, callback);
       },
-      notes: "Steep trail!",
-      pics: ["http://cs7002.vk.me/c540103/v540103264/1e9d6/4cq4et75xmA.jpg", 
-        "https://c2.staticflickr.com/8/7049/6863925343_9ac9c68bb4_z.jpg",
-        "https://c2.staticflickr.com/6/5281/5298818285_985bcf0b40_z.jpg", 
-        "http://lh6.ggpht.com/-0WqtCkikIAw/Ur5y02is7NI/AAAAAAAAuuw/RntKw1VQA1Q/haiku-stairs-3%25255B2%25255D.jpg"],
-      parentMaps: [3, 4],
-    });
-
-    places.insert({
-      name: "Bean",
-      location: {
-        lat: 41.883456,
-        lng: -87.623132
+      function(callback) {
+        maps.insert({
+          _id: 2,
+          name: "Washington D.C."
+        }, callback);        
       },
-      notes: "",
-      pics: ["http://dvogled.rs/wp-content/uploads/2014/08/chicagosculpture.jpg"], 
-      parentMaps: [1]
-    });
+      function(callback) {
 
-    places.insert({
-      name: "Skydeck",
-      location: {
-        lat: 41.879587,
-        lng: -87.636159
+        maps.insert({
+          _id: 3,
+          name: "USA"
+        }, callback);        
       },
-      notes: "",
-      pics: ["http://33.media.tumblr.com/2a985a4179e6cd8ab916325d79075ee4/tumblr_mw2p7hmE3X1rshyy2o1_1280.jpg"], 
-      parentMaps: [1]
-    });
+      function(callback) {
 
-    console.log("done");
+        maps.insert({
+          _id: 4,
+          name: "Mountain trails"
+        }, callback);        
+      },
+      function(callback) {
 
-    db.close();
+        places.insert({
+          name: "Haiku Stairs",
+          location: {
+            lat: 21.400788,
+            lng: -157.821934
+          },
+          notes: "Steep trail!",
+          pics: ["http://cs7002.vk.me/c540103/v540103264/1e9d6/4cq4et75xmA.jpg", 
+            "https://c2.staticflickr.com/8/7049/6863925343_9ac9c68bb4_z.jpg",
+            "https://c2.staticflickr.com/6/5281/5298818285_985bcf0b40_z.jpg", 
+            "http://lh6.ggpht.com/-0WqtCkikIAw/Ur5y02is7NI/AAAAAAAAuuw/RntKw1VQA1Q/haiku-stairs-3%25255B2%25255D.jpg"],
+          parentMaps: [3, 4],
+        }, callback);        
+      },
+      function(callback) {
+
+        places.insert({
+          name: "Bean",
+          location: {
+            lat: 41.883456,
+            lng: -87.623132
+          },
+          notes: "",
+          pics: ["http://dvogled.rs/wp-content/uploads/2014/08/chicagosculpture.jpg"], 
+          parentMaps: [1]
+        }, callback);
+        
+      },
+      function(callback) {
+        places.insert({
+          name: "Skydeck",
+          location: {
+            lat: 41.879587,
+            lng: -87.636159
+          },
+          notes: "",
+          pics: ["http://33.media.tumblr.com/2a985a4179e6cd8ab916325d79075ee4/tumblr_mw2p7hmE3X1rshyy2o1_1280.jpg"], 
+          parentMaps: [1]
+        }, callback);
+      },
+      function(callback) {
+        console.log("done");
+        db.close();
+      }
+    ];
+
+    var run = function(todoFuns, i) {
+      todoFuns[i] ( function() {
+        run(todoFuns, i + 1);
+      } );
+    };
+
+    run(todo, 0);
   });
 }
 
